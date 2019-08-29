af73f9783f4a186b0486b69b77eff1d1f1923095
Jonathan <jonathanbek93@gmail.com>
2019-08-29 14:27:03 +0800

    did basic

--- script.jsx
+++ script.jsx
@@ -1,3 +1,18 @@
+class ListItems extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        let list = this.props.list.map((item, index)=>{
+            return <li key={index}>{item}</li>
+        })
+
+        return(
+            <ul>
+                {list}
+            </ul>
+    )};
+}
 class List extends React.Component {
   constructor(){
     super()
@@ -9,12 +24,24 @@ class List extends React.Component {
   }
 
   addItem(){
-    debugger;
+    console.log('adding item');
+    let word = this.state.word;
+    console.log("word:", word);
+    let list = this.state.list.concat(word)
+    console.log("list:", list);
+    this.setState({list: list});
+    this.setState({word: ""});
   }
 
   changeHandler(){
-    debugger;
+    let input = event.target.value;
+    console.log('input:', input)
+    let word = this.state.word;
+    word = input;
+    this.setState({word: word});
   }
+
+
 
   render() {
       // render the list with a map() here
@@ -24,6 +51,7 @@ class List extends React.Component {
         <div className="list">
           <input onChange={()=>{this.changeHandler()}} value={this.state.word}/>
           <button onClick={()=>{this.addItem()}}>add item</button>
+          <ListItems list={this.state.list}/>
         </div>
       );
   }
@@ -33,4 +61,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
