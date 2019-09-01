0ba3917b4a764386ed0e6a4a7ce770a860f9c1cf
Jonathan <jonathanbek93@gmail.com>
2019-09-01 21:20:04 +0800

    did edit item input

--- script.jsx
+++ script.jsx
@@ -1,22 +1,37 @@
 class ListItems extends React.Component{
 
+    editItem(event, index){
+        this.props.editItem(event, index);
+    }
+
     deleteItem(index){
         this.props.deleteItem(index);
     }
 
-    timeSince(item){
+    timeSinceCreated(item){
         return moment(item.created_at).fromNow();
     }
+
+    timeSinceUpdated(item){
+        return moment(item.updated_at).fromNow();
+    }
+
 
     render(){
         let list = this.props.list.map((item, index)=>{
             return(
-                <li key={index} onClick={() => {this.deleteItem(index)}}>Text: {item.text} Posted: {this.timeSince(item)}</li>
+                <li className="list-group-item col-4 offset-2 w-100" key={index}>
+                    <small className="w-100">Updated: {this.timeSinceUpdated(item)}</small>
+                    <div className="row">
+                        <input className="col-8" defaultValue={item.text} onChange={() => {this.editItem(event, index)}}></input>
+                        <button className="col-2 offset-2 btn btn-light" onClick={() => {this.deleteItem(index)}}><small>Remove</small></button>
+                    </div>
+                </li>
              )
         });
 
         return(
-            <ul>
+            <ul className="list-group row">
                 {list}
             </ul>
     )};
@@ -28,7 +43,12 @@ class DeletedItems extends React.Component{
     }
     render(){
         let deleteList = this.props.deleteList.map((item, index) => {
-            return <li key={index}>Text: {item.text} Deleted: {this.timeSince(item)}</li>
+            return (
+                <li key={index}>
+                    <p>Text: {item.text} </p>
+                    <p>Deleted: {this.timeSince(item)}</p>
+                </li>
+                )
         });
         return(
             <ul>
@@ -52,6 +72,19 @@ class List extends React.Component {
       deleteList: [],
       error: ""
     }
+  }
+
+  editItem(event, index){
+    console.log('editing item!');
+    console.log('edited text:', event.target.value)
+    let list = [...this.state.list];
+    let item = list[index];
+    console.log('item to save to:', item);
+    item.text = event.target.value;
+    this.setState({list: list});
+    console.log('state after edit:', this.state.list[index])
+
+
   }
 
   deleteItem(index){
@@ -116,14 +149,24 @@ class List extends React.Component {
   render() {
       console.log(moment().format())
       return (
-        <div className="list">
-          <input onChange={()=>{this.changeHandler()}} value={this.state.item.text}/>
-          <button onClick={()=>{this.addItem()}}>add item</button>
-          <p>{this.state.error}</p>
-          <p>Items Count: {this.state.list.length}</p>
-          <ListItems deleteItem={(index)=>{this.deleteItem(index)}} list={this.state.list}/>
-          <p>Deleted Items Count: {this.state.deleteList.length}</p>
-          <DeletedItems deleteList={this.state.deleteList}/>
+        <div className="everything-container">
+            <div className="form row mt-5 w-100">
+                <input className="col-4 offset-3" onChange={()=>{this.changeHandler()}} value={this.state.item.text}/>
+                <button className="col-1" onClick={()=>{this.addItem()}}>add item</button>
+            </div>
+            <div className="error row">
+                <p className="col-4 offset-3">{this.state.error}</p>
+            </div>
+
+            <div className="list">
+                <p>Items Count: {this.state.list.length}</p>
+                <ListItems deleteItem={(index)=>{this.deleteItem(index)}} editItem={(event, index)=>{this.editItem(event, index)}} list={this.state.list}/>
+            </div>
+
+            <div className="deleted-list">
+                <p>Deleted Items Count: {this.state.deleteList.length}</p>
+                <DeletedItems deleteList={this.state.deleteList}/>
+            </div>
         </div>
       );
   }
