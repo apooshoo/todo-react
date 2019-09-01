e4862e000b5fd84ac22eae799514ec264fb970ce
Jonathan <jonathanbek93@gmail.com>
2019-09-01 23:00:56 +0800

    did edit error, light css

--- script.jsx
+++ script.jsx
@@ -16,6 +16,10 @@ class ListItems extends React.Component{
         return moment(item.updated_at).fromNow();
     }
 
+    fillCheckBox(event, index){
+        this.props.fillCheckBox(event, index);
+    }
+
 
     render(){
         let list = this.props.list.map((item, index)=>{
@@ -23,8 +27,12 @@ class ListItems extends React.Component{
                 <li className="list-group-item col-4 offset-2 w-100" key={index}>
                     <small className="w-100">Updated: {this.timeSinceUpdated(item)}</small>
                     <div className="row">
-                        <input className="col-8" defaultValue={item.text} onChange={() => {this.editItem(event, index)}}></input>
-                        <button className="col-2 offset-2 btn btn-light" onClick={() => {this.deleteItem(index)}}><small>Remove</small></button>
+                        <img className="col-2 checkbox" onClick={()=>{this.fillCheckBox(event, index)}} src="checkbox-regular-24.png"/>
+                        <input className="col-8" value={item.text} onChange={() => {this.editItem(event, index)}}></input>
+                        <img className="col-2 delete-icon" onClick={() => {this.deleteItem(index)}} src="trash-regular-24.png"></img>
+                    </div>
+                    <div className="row">
+                        <small className="col offset-2">{item.error}</small>
                     </div>
                 </li>
              )
@@ -38,15 +46,28 @@ class ListItems extends React.Component{
 }
 
 class DeletedItems extends React.Component{
-    timeSince(item){
+    timeSinceUpdated(item){
         return moment(item.updated_at).fromNow();
     }
+
+    fillCheckBox(event, item){
+        let checkBox = event.target;
+        if(item.done === false){
+            return 'checkbox-checked-regular-24.png';
+        } else{
+            return 'checkbox-regular-24.png';
+        }
+    }
+
     render(){
         let deleteList = this.props.deleteList.map((item, index) => {
             return (
-                <li key={index}>
-                    <p>Text: {item.text} </p>
-                    <p>Deleted: {this.timeSince(item)}</p>
+                <li className="list-group-item col-4 offset-2 w-100" key={index}>
+                    <small className="w-100">Deleted: {this.timeSinceUpdated(item)}</small>
+                    <div className="row">
+                        <img className="col-2 checkbox" src={this.fillCheckBox(event, item)}/>
+                        <p className="col-8">{item.text}</p>
+                    </div>
                 </li>
                 )
         });
@@ -66,7 +87,8 @@ class List extends React.Component {
         text: "",
         done: null,
         created_at: null,
-        updated_at: null
+        updated_at: null,
+        error: null
       },
       list : [],
       deleteList: [],
@@ -74,17 +96,40 @@ class List extends React.Component {
     }
   }
 
+  fillCheckBox(event, index){
+    let checkBox = event.target;
+    console.log("checkbox:", event.target)
+    let list = [...this.state.list];
+    let item = list[index];
+    console.log('item to edit:', item);
+    if(item.done === false){
+        console.log('checking!');
+        checkBox.setAttribute('src', 'checkbox-checked-regular-24.png');
+        item.done =true;
+        this.setState({list: list});
+    } else{
+        console.log('unchecking!');
+        checkBox.setAttribute('src', 'checkbox-regular-24.png');
+        item.done = false;
+        this.setState({list: list});
+    };
+  }
+
   editItem(event, index){
     console.log('editing item!');
-    console.log('edited text:', event.target.value)
+    console.log('edited text:', event.target.value);
     let list = [...this.state.list];
     let item = list[index];
     console.log('item to save to:', item);
-    item.text = event.target.value;
-    this.setState({list: list});
-    console.log('state after edit:', this.state.list[index])
-
-
+    if(event.target.value.length <= 10){
+        item.error = null;
+        item.text = event.target.value;
+        this.setState({list: list});
+        console.log('text after edit:', this.state.list[index].text);
+    }else{
+        item.error = "error: max chars: 10!";
+        this.setState({list: list});
+    }
   }
 
   deleteItem(index){
@@ -120,8 +165,9 @@ class List extends React.Component {
         text: "",
         done: null,
         created_at: null,
-        updated_at: null
-      }});
+        updated_at: null,
+        error: null
+    }});
   }
 
   changeHandler(){
@@ -160,7 +206,7 @@ class List extends React.Component {
 
             <div className="list">
                 <p>Items Count: {this.state.list.length}</p>
-                <ListItems deleteItem={(index)=>{this.deleteItem(index)}} editItem={(event, index)=>{this.editItem(event, index)}} list={this.state.list}/>
+                <ListItems deleteItem={(index)=>{this.deleteItem(index)}} editItem={(event, index)=>{this.editItem(event, index)}} fillCheckBox={(event, index)=>{this.fillCheckBox(event, index)}} list={this.state.list}/>
             </div>
 
             <div className="deleted-list">
